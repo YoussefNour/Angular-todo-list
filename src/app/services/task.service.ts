2393c56e6f18973df45cde2a35d09c89b16e23b9
Youssef <youssef.ossama2@gmail.com>
2021-07-15 19:22:58 +0200

    chore: Prefective Maintenace
    - introduces host URL to enviroment
    
    - some minor styling changes

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task } from '../models/Task';
+import { environment } from 'src/environments/environment';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
-  })
+    'Content-Type': 'application/json',
+  }),
 };
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks';
+  private apiUrl = environment.apiUrl + 'tasks';
   constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
@@ -25,20 +26,20 @@ export class TaskService {
     return this.http.delete<Task>(url);
   }
 
-  toggleTaskReminder(task: Task): Observable<Task>{
+  toggleTaskReminder(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.put<Task>(url, task, httpOptions);
   }
 
-  addTask(task: Task): Observable<Task>{
+  addTask(task: Task): Observable<Task> {
     return this.http.post<Task>(this.apiUrl, task, httpOptions);
   }
 
-  sortTasks(tasks: Task[]): Observable<Task[]>{
+  sortTasks(tasks: Task[]): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl + '?_sort=done&_order=asc');
   }
 
-  searchTask(query: string): Observable<Task[]>{
+  searchTask(query: string): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl + `?q=${query}`);
   }
 }
